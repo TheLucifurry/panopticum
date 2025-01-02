cf11ce98b82ded68359c292bdb4d66ad0ed9f260
Lucifurry <thelucifurry@gmail.com>
2025-01-02 16:33:16 +0700

    Fix `ui`: hide hours for short durations in `toDurationStringFromSeconds` util

--- ws/ui/src/widgets/utils/datetime.ts
+++ ws/ui/src/widgets/utils/datetime.ts
@@ -2,9 +2,10 @@
 const toTwoDigitMin = (s: string | number) => s.toString().padStart(2, '0')
 const ceil = Math.trunc
 
+const SECS_IN_HOUR = 60 * 60
+
 export function toDurationStringFromSeconds(seconds: number | undefined): string {
   return seconds != null
-    ? `${ceil(seconds / 3600)}:${toTwoDigitMin(ceil(seconds / 60))}:${toTwoDigitMin(ceil(seconds % 60))}`
+    ? `${seconds < SECS_IN_HOUR ? '' : ceil(seconds / 3600) + ':'}${toTwoDigitMin(ceil(seconds / 60))}:${toTwoDigitMin(ceil(seconds % 60))}`
     : "--:--"
-  // return seconds != null ? new Date(1000 * seconds).toISOString().slice(14, 19) : '--:--'
 }
