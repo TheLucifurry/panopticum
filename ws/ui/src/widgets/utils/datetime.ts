cbbda412e5b930dbdc7888444e2ba6072422ee0a
Lucifurry <thelucifurry@gmail.com>
2025-01-02 15:58:40 +0700

    Ref `ui`: stupidify `toDurationStringFromSeconds` util

--- ws/ui/src/widgets/utils/datetime.ts
+++ ws/ui/src/widgets/utils/datetime.ts
@@ -1,4 +1,10 @@
 
+const toTwoDigitMin = (s: string | number) => s.toString().padStart(2, '0')
+const ceil = Math.trunc
+
 export function toDurationStringFromSeconds(seconds: number | undefined): string {
-  return seconds != null ? new Date(1000 * seconds).toISOString().slice(14, 19) : '--:--'
+  return seconds != null
+    ? `${ceil(seconds / 3600)}:${toTwoDigitMin(ceil(seconds / 60))}:${toTwoDigitMin(ceil(seconds % 60))}`
+    : "--:--"
+  // return seconds != null ? new Date(1000 * seconds).toISOString().slice(14, 19) : '--:--'
 }
