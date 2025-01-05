57be8138ff2927a286b7d75c32df640fb9a0d99c
Lucifurry <thelucifurry@gmail.com>
2025-01-05 10:45:12 +0700

    Feat `core`: add more video formats to `ACCEPTABLE_VIDEO_FORMATS`

--- ws/core/src/consts.rs
+++ ws/core/src/consts.rs
@@ -5,4 +5,8 @@ pub const ACCEPTABLE_AUDIO_FORMATS: ConstVecString = &[
     "wv", "wvc", "wvp", "flac", "aiff",
 ];
 
-pub const ACCEPTABLE_VIDEO_FORMATS: ConstVecString = &["webm"];
+pub const ACCEPTABLE_VIDEO_FORMATS: ConstVecString = &[
+    "mkv", "avi", "mov", "mp4", "m4v", "flv", "wmv", "ogv", "3gp", "3g2", "mts",
+    "m2t", "m2p", "m2a", "m2v", "m1v", "m1a",
+    "ts", "webm", "m2ts", "mpeg",
+];
