453cf0e871897239ca47499fd06147670568049c
Lucifurry <thelucifurry@gmail.com>
2025-01-05 10:43:46 +0700

    Feat `ui`: add `useIncrementable` composable

---
+++ ws/ui/src/shared/composables/useIncrementable.ts
@@ -0,0 +1,21 @@
+import type { Ref } from 'vue'
+import { clampNumber } from 'webshrine'
+
+interface IRefIncrementableOptions {
+  step?: number
+  max?: number
+  min?: number
+}
+
+export function useIncrementable<T extends number = number>(targetTef: Ref<T, T>, options: IRefIncrementableOptions = {}) {
+  const { step = 1, max = Number.MAX_SAFE_INTEGER, min = Number.MIN_SAFE_INTEGER } = options
+
+  const inc = (stepMultiplier = 1) => {
+    targetTef.value = clampNumber(targetTef.value + step * stepMultiplier, min, max) as T
+  }
+
+  return {
+    inc,
+    dec: (stepMultiplier = -1) => inc(stepMultiplier),
+  }
+}
