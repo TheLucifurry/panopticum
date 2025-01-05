1e21f0b7a96e5224547a2c52cd54d33e9f8c7df2
Lucifurry <thelucifurry@gmail.com>
2025-01-05 09:36:48 +0700

    Ref `ui`: remove `pinia` from deps

--- ws/ui/src/app/index.ts
+++ ws/ui/src/app/index.ts
@@ -1,5 +1,4 @@
 import Router from '@/pages'
-import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import { debug } from './debug'
 import App from './index.vue'
@@ -9,9 +8,6 @@ import './styles/global.css'
 if (import.meta.env.DEV)
   debug()
 
-const Pinia = createPinia()
-
 createApp(App)
   .use(Router)
-  .use(Pinia)
   .mount('#root')
