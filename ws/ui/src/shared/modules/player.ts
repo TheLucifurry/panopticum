010849176f8d52a6417742d25d51a3eec7e80772
Lucifurry <thelucifurry@gmail.com>
2025-01-10 20:59:45 +0700

    Other `ui`: change fallback media length time

--- ws/ui/src/shared/modules/player.ts
+++ ws/ui/src/shared/modules/player.ts
@@ -5,6 +5,7 @@ import { useToggle } from '@vueuse/core'
 import { defineModule } from '@webshrine/vue'
 import { shallowRef } from 'vue'
 
+export const DEFAULT_TRACK_LENGTH_TIME = 4 * 60 + 4
 export const DEFAULT_VOLUME = 0.5
 export const DEFAULT_VOLUME_INC_STEP = 0.05
 export const DEFAULT_PLAYBACK_SPEED = 1
@@ -26,7 +27,7 @@ export const usePlayer = defineModule(() => {
     max: DEFAULT_PLAYBACK_SPEED_RANGE[1],
   })
 
-  const trackLengthTime = shallowRef(500)
+  const trackLengthTime = shallowRef(DEFAULT_TRACK_LENGTH_TIME)
   const currentTime = shallowRef(DEFAULT_PLAYBACK_SPEED)
   const currentTimeChange = useIncrementable(currentTime, {
     step: 5,
