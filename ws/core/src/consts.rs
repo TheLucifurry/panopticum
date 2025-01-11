ccfd5dbec13f7c299abd78b2ac932e92056d0ee9
Lucifurry <thelucifurry@gmail.com>
2025-01-11 19:02:05 +0700

    Feat `core`: add `opus` to supportable audio formats

--- ws/core/src/consts.rs
+++ ws/core/src/consts.rs
@@ -2,7 +2,7 @@ pub type ConstVecString = &'static [&'static str];
 
 pub const ACCEPTABLE_AUDIO_FORMATS: ConstVecString = &[
     "wav", "mp3", "ogg", "aac", "m4a", "wma", "ape", "dsd", "dsf", "dff", "dts", "mpc", "msv",
-    "wv", "wvc", "wvp", "flac", "aiff",
+    "wv", "wvc", "wvp", "flac", "aiff", "opus"
 ];
 
 pub const ACCEPTABLE_VIDEO_FORMATS: ConstVecString = &[
