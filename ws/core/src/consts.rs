a2ed290c466811b51f64ef7062d98356bc87062d
Lucifurry <thelucifurry@gmail.com>
2025-01-12 19:55:45 +0700

    Ref `core`: remove unsupported by web engines file formats

--- ws/core/src/consts.rs
+++ ws/core/src/consts.rs
@@ -1,12 +1,9 @@
 pub type ConstVecString = &'static [&'static str];
 
 pub const ACCEPTABLE_AUDIO_FORMATS: ConstVecString = &[
-    "wav", "mp3", "ogg", "aac", "m4a", "wma", "ape", "dsd", "dsf", "dff", "dts", "mpc", "msv",
-    "wv", "wvc", "wvp", "flac", "aiff", "opus"
+    "wav", "mp3", "ogg", "aac", "m4a", "flac", "opus"
 ];
 
 pub const ACCEPTABLE_VIDEO_FORMATS: ConstVecString = &[
-    "mkv", "avi", "mov", "mp4", "m4v", "flv", "wmv", "ogv", "3gp", "3g2", "mts",
-    "m2t", "m2p", "m2a", "m2v", "m1v", "m1a",
-    "ts", "webm", "m2ts", "mpeg",
+    "mkv", "avi", "mov", "mp4", "m4v", "flv", "wmv", "ts", "webm", "mpeg",
 ];
