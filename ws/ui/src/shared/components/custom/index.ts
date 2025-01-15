2446aaff437c5460c412ac132a75ef1486102373
Lucifurry <thelucifurry@gmail.com>
2025-01-15 12:42:02 +0700

    Feat `ui`: add `Img` component

--- ws/ui/src/shared/components/custom/index.ts
+++ ws/ui/src/shared/components/custom/index.ts
@@ -1,4 +1,5 @@
 export { default as Bezel } from './primitives/Bezel.vue'
 export { default as Group } from './primitives/Group.vue'
+export { default as Img } from './primitives/Img.vue'
 export { default as Panel } from './primitives/Panel.vue'
 export { default as BezelProvider } from './providers/BezelProvider.vue'
---
+++ ws/ui/src/shared/components/custom/primitives/Img.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { shallowRef } from 'vue'
+
+interface Props {
+  src: string
+  srcFallback?: string
+  alt?: string
+}
+
+const p = defineProps<Props>()
+
+const currentSrc = shallowRef(p.src)
+
+function handleError() {
+  currentSrc.value = p.srcFallback || ''
+}
+</script>
+
+<template>
+  <img
+    :src="currentSrc"
+    :alt="p.alt"
+    @error="handleError"
+  >
+</template>
