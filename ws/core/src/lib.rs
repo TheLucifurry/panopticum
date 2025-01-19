1946323c4620999d288960f6e0dd1fa983ef47dd
Lucifurry <thelucifurry@gmail.com>
2025-01-19 10:37:57 +0700

    Ref `core`: change `Modules` setup place

--- ws/core/src/lib.rs
+++ ws/core/src/lib.rs
@@ -1,4 +1,5 @@
-use modules::AppState;
+use modules::Modules;
+use tauri::Manager;
 
 pub mod consts;
 pub mod utils;
@@ -6,10 +7,13 @@ pub mod modules;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let state = AppState::new();
 
     tauri::Builder::default()
-        .manage(state)
+        .setup(|app| {
+            let modules = Modules::new(app);
+            app.manage(modules);
+            Ok(())
+        })
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(modules::invoke_handler)
