8ff29d6037f9324384655b0aa73f130557f2c2f1
Lucifurry <thelucifurry@gmail.com>
2025-01-22 11:15:53 +0700

    Ref `schemas`: move output ts file to src, add possibility to add custom ts code

---
+++ ws/schemas/src/index.ts
@@ -0,0 +1,16 @@
+import type { ContentNode, IContentList, IContentMedia } from './gen'
+
+export * from './gen'
+
+export interface ContentNodeWithMedia {
+  type: 'media'
+  body: IContentMedia
+}
+
+export interface ContentNodeWithList {
+  type: 'list'
+  body: IContentList
+}
+
+export const isContentNodeWithMedia = (v: ContentNode): v is ContentNodeWithMedia => v.type === 'media'
+export const isContentNodeWithList = (v: ContentNode): v is ContentNodeWithList => v.type === 'media'
