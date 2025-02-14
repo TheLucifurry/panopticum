5388e942242bd6f55a8a4db79c20af22554d5f20
Lucifurry <thelucifurry@gmail.com>
2025-02-14 10:03:50 +0700

    Ref `schema`: split schemas, sort by libs №2

--- ws/schemas/src/index.ts
+++ ws/schemas/src/index.ts
@@ -1,23 +1,2 @@
-import type { ContentNode, IContentList, IContentMedia, IContentPreview } from './gen'
-
 export * from './gen'
 export * from './libs'
-
-export interface ContentNodeWithMedia {
-  type: 'media'
-  body: IContentMedia
-}
-
-export interface ContentNodeWithList {
-  type: 'list'
-  body: IContentList
-}
-
-export interface ContentNodeWithPreview {
-  type: 'preview'
-  body: IContentPreview
-}
-
-export const isContentNodeWithMedia = (v: ContentNode): v is ContentNodeWithMedia => v.type === 'media'
-export const isContentNodeWithList = (v: ContentNode): v is ContentNodeWithList => v.type === 'list'
-export const isContentNodeWithPreview = (v: ContentNode): v is ContentNodeWithPreview => v.type === 'preview'
---
+++ ws/schemas/src/libs/content_node.ts
@@ -0,0 +1,19 @@
+import type { ContentNode, IContentList, IContentMedia, IContentPreview } from '../gen'
+
+export interface ContentNodeWithMedia {
+  type: 'media'
+  body: IContentMedia
+}
+
+export interface ContentNodeWithList {
+  type: 'list'
+  body: IContentList
+}
+export interface ContentNodeWithPreview {
+  type: 'preview'
+  body: IContentPreview
+}
+
+export const isContentNodeWithMedia = (v: ContentNode): v is ContentNodeWithMedia => v.type === 'media'
+export const isContentNodeWithList = (v: ContentNode): v is ContentNodeWithList => v.type === 'list'
+export const isContentNodeWithPreview = (v: ContentNode): v is ContentNodeWithPreview => v.type === 'preview'
