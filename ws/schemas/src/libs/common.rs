2f6b5bfe7e2810480d8668509dc5b12af52cdd80
Lucifurry <thelucifurry@gmail.com>
2025-02-14 13:04:29 +0700

    Feat `schema`: add impl of `as_str` for `PathNode` model

--- ws/schemas/src/libs/common.rs
+++ ws/schemas/src/libs/common.rs
@@ -2,16 +2,25 @@ use serde::{Deserialize, Serialize};
 use typeshare::typeshare;
 
 // Cant be converted by typeshare
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ValueLabelPair(String, String);
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct ValueLabelPair(pub String, pub String);
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[serde(untagged)]
 pub enum PathNode {
     // Variant for a single string
     String(String),
     // Variant for a pair of strings
     Pair(ValueLabelPair),
+}
+
+impl PathNode {
+    pub fn as_str(&self) -> &str {
+        match self {
+            PathNode::String(s) => s.as_str(),
+            PathNode::Pair(ValueLabelPair(first, _)) => first.as_str(),
+        }
+    }
 }
 
 pub type PathNodes = Vec<PathNode>;
