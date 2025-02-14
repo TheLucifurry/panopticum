915871b3ebf09d81f8e868cbbdfe9c41e4c34f1f
Lucifurry <thelucifurry@gmail.com>
2025-02-14 10:34:29 +0700

    Feat `schema`: add `isPathNodeAValueLabelPair` type guard

--- ws/schemas/src/libs/common.ts
+++ ws/schemas/src/libs/common.ts
@@ -3,3 +3,5 @@ export type ValueLabelPair = [value: string, label: string]
 export type PathNode = string | ValueLabelPair
 
 export type PathNodes = PathNode[]
+
+export const isPathNodeAValueLabelPair = Array.isArray as (v: PathNode) => v is ValueLabelPair
