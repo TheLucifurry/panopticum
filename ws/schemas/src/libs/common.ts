730607cc12041da2107d7caa754d14e26fa45ecb
Lucifurry <thelucifurry@gmail.com>
2025-02-15 18:42:44 +0700

    Ref `schema`: methods for `PathNode` model

--- ws/schemas/src/libs/common.ts
+++ ws/schemas/src/libs/common.ts
@@ -2,6 +2,10 @@ export type ValueLabelPair = [value: string, label: string]
 
 export type PathNode = string | ValueLabelPair
 
-export type PathNodes = PathNode[]
+export namespace PathNode {
+  export const isValueLabelPair = Array.isArray as (v: PathNode) => v is ValueLabelPair
+  export const toValue = (v: PathNode) => typeof v === 'string' ? v : v[0]
+  export const toLabel = (v: PathNode) => typeof v === 'string' ? v : v[1]
+}
 
-export const isPathNodeAValueLabelPair = Array.isArray as (v: PathNode) => v is ValueLabelPair
+export type PathNodes = PathNode[]
