b33cecfe0ee57482e60eaffb9ab3bffbc87728e6
Lucifurry <thelucifurry@gmail.com>
2025-02-17 12:43:45 +0700

    Build: disable some eslint rules to support TS namespaces

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   typescript: true,
   vue: true,
+  ignores: ['node_modules'],
+  rules: {
+    'ts/no-namespace': 'off',
+    'ts/no-redeclare': 'off',
+  },
 })
