2709d58fdcb9fa2afbca27f9eb2a50e0332a8edc
Lucifurry <thelucifurry@gmail.com>
2025-02-17 12:38:50 +0700

    Feat `ui`: add basic location indication via `Breadcrumb` components

--- ws/ui/src/pages/p/[id].vue
+++ ws/ui/src/pages/p/[id].vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import type { PathNodes } from '@panopticum/schemas'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '@/shared/components/ui/breadcrumb'
 import { useQueryParam } from '@/shared/composables'
 import { useContent, useInteraction } from '@/shared/modules'
 import { ProviderViewMain } from '@/widgets/provider'
-import { computed, watchEffect } from 'vue'
+import { PathNode } from '@panopticum/schemas'
+import { computed, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 definePage({
@@ -28,17 +30,31 @@ i10.page.defineTitle(providerName)
 function onGoLocation(newPath: PathNodes) {
   locationPath.value = newPath
 }
+
+watch(providerKey, () => {
+  locationPath.value = null
+})
 </script>
 
 <template>
   <div>
-    <teleport defer to='#app__subtitle'>
-      <span v-for="(n, ix) in locationPath" :key="ix">
-        {{ n }}
-      </span>
+    <teleport defer to="#app__subtitle">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <template v-for="(node, ix) in locationPath" :key="ix">
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink>
+                {{ PathNode.toLabel(node) }}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </template>
+        </BreadcrumbList>
+      </Breadcrumb>
     </teleport>
     <ProviderViewMain
       v-if="providerKey === '$local_files'"
+      :location-path="locationPath || undefined"
       @go-location="onGoLocation"
     />
   </div>
